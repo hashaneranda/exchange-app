2f755ba0ffb0c4fefc3ffdde0bcb06b914a8cd9e
Hashan Eranda <hashaneranda@gmail.com>
2022-01-17 20:50:55 +0530

    tests updated

--- service-auth/test/app.e2e-spec.ts
+++ service-auth/test/app.e2e-spec.ts
@@ -14,10 +14,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ping (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/ping')
       .expect(200)
-      .expect('Hello World!');
+      .expect('pong!');
   });
 });
